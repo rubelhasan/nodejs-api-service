a1b5f87023a2f94a95b404115f132ffbe17f786b
Rubel Hasan <rubeldhkbd@gmail.com>
2018-01-25 13:50:15 +0600

    added product route

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const router = express.Router()
 const Auth = require('./auth')
 const apiEndpoint = process.env.API_ENDPOINT || '3000'
 
-router.get('/api/product/list', function (req, res) {
-  res.send('product list')
-})
 router.get('/login', (req, res) => {
   var token = Auth.getAuthToken({email: 'rubeldhkbd@gmail.com'})
   res.send(token)
@@ -15,6 +12,21 @@ router.get('/logout', (req, res) => {
   res.send('logOut true')
 })
 
+router.get('/api/product/add', function (req, res) {
+  res.send('product add')
+})
+
+router.get('/api/product/list', function (req, res) {
+  res.send('product list')
+})
+
+router.get('/api/product/update', function (req, res) {
+  res.send('product update')
+})
+
+router.get('/api/product/delete', function (req, res) {
+  res.send('product delete')
+})
 app.use((req, res, next) => {
   Auth.isAuthenticated(req, res, next)
 })
